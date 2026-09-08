8ff21acf847615106802418d2daa2be840353e17
wendellsantos2 <wemdellcos@gmail.com>
2026-09-07 23:20:16 -0300

    feat: criar projeto portal-web em Astro integrado a Vercel, Web Admin e APK Android

---
+++ portal-web/astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
+});
